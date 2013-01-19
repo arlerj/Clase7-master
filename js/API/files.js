ce2b7510791d50e8e81d83f312aa5b2a32fffcc5
Alejandro <alejmlara@me.com>
2013-01-18 20:02:52 -0600

    Código para escritura de archivos

--- js/API/files.js
+++ js/API/files.js
@@ -4,7 +4,21 @@ $(document).ready(function(e) {
 		$('#archivos .rounded li').tap(function(){
 			switch($(this).index()){
 				case 0://Escribir (Crear) Archivos
-					
+					window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+					function gotFS(fileSystem) {
+        fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+    }
+
+    function gotFileEntry(fileEntry) {
+        fileEntry.createWriter(gotFileWriter, fail);
+    }
+
+    function gotFileWriter(writer) {
+        writer.onwriteend = function(evt) {
+			alert("El archivo se ha escrito");
+        };
+        writer.write($('#archivosCamp').val());
+    }
 					break;
 				case 1://Leer Archivo
 					
